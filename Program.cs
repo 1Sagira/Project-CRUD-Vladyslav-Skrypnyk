d05207b1a8b1cf8b29e02d4b95c4539c913afa0b
Sagira <102478767+1Sagira@users.noreply.github.com>
2025-10-24 14:19:16 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -14,6 +14,13 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+ var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    
+dbContext.Database.Migrate();
+}
+
 
 if (app.Environment.IsDevelopment())
 {
