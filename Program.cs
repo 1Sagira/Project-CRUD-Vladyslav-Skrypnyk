e0da982510b9b8a8f48a3b6acfcee4580ee821d3
Sagira <102478767+1Sagira@users.noreply.github.com>
2025-10-30 22:57:50 +0100

    Fix

--- Program.cs
+++ Program.cs
@@ -4,14 +4,14 @@ using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// 1. Налаштування CORS
+
 const string MyAllowSpecificOrigins = "AllowMyFrontend";
 builder.Services.AddCors(options =>
 {
     options.AddPolicy(name: MyAllowSpecificOrigins,
                       policy =>
                       {
-                          // ВАЖЛИВО: Переконайтеся, що тут правильний URL вашого фронтенду
+                         
                           policy.WithOrigins("https://project-crud-vladyslav-skrypnyk2.onrender.com") 
                                 .AllowAnyHeader()
                                 .AllowAnyMethod();
@@ -24,8 +24,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// *** ПОКРАЩЕНА ЛОГІКА ПОРТУ ***
-// Цей блок коду буде виконуватися ТІЛЬКИ на сервері (не локально)
+
 if (!builder.Environment.IsDevelopment())
 {
     builder.WebHost.ConfigureKestrel(serverOptions =>
@@ -33,22 +32,22 @@ if (!builder.Environment.IsDevelopment())
         var portEnv = Environment.GetEnvironmentVariable("PORT");
         if (int.TryParse(portEnv, out int port))
         {
-            // Слухаємо на 0.0.0.0 (всі IP) та порту, який дав Onrender
+            
             serverOptions.Listen(IPAddress.Any, port);
         }
     });
 }
-// Локально (IsDevelopment()) він автоматично використає 'launchSettings.json'
-// *** КІНЕЦЬ ЗМІН ***
+
+
 
 
 var app = builder.Build();
 
-// Застосовуємо міграції бази даних при запуску
+
 using (var scope = app.Services.CreateScope())
 {
     var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    // Цей рядок все ще може викликати збій, якщо ви не використовуєте "Диск"
+    
     dbContext.Database.Migrate(); 
 }
 
@@ -61,7 +60,7 @@ if (app.Environment.IsDevelopment())
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
-// 2. Активуємо CORS
+
 app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
